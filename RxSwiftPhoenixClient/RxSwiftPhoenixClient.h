5646b54d6d0804d711625c3f6de201ce8d2dc131
dsrees <daniel.rees18@gmail.com>
2022-02-07 10:46:51 -0500

    Setting up project with Carthage

---
+++ RxSwiftPhoenixClient/RxSwiftPhoenixClient.h
@@ -0,0 +1,18 @@
+//
+//  RxSwiftPhoenixClient.h
+//  RxSwiftPhoenixClient
+//
+//  Created by Daniel Rees on 2/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxSwiftPhoenixClient.
+FOUNDATION_EXPORT double RxSwiftPhoenixClientVersionNumber;
+
+//! Project version string for RxSwiftPhoenixClient.
+FOUNDATION_EXPORT const unsigned char RxSwiftPhoenixClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwiftPhoenixClient/PublicHeader.h>
+
+
